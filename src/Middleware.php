3ee9914c0dc3169ef27a725819abcac7c3e044cb
paolocatalan <paolo_catalan@yahoo.com>
2024-11-08 17:02:32 +0800

    added middleware file

---
+++ src/Middleware.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+use DI\Container;
+use Slim\App;
+use App\Middleware\AddJsonResponseHeader;
+use Slim\Handlers\Strategies\RequestResponseArgs;
+
+return function(App $app, Container $container) {
+    $app->addBodyParsingMiddleware();
+    $collector = $app->getRouteCollector();
+    $collector->setDefaultInvocationStrategy(new RequestResponseArgs);
+
+    $errorMiddlleware = $app->addErrorMiddleware(true, true, true);
+    $errorHandler = $errorMiddlleware->getDefaultErrorHandler();
+    $errorHandler->forceContentType('application/json');
+
+
+    $app->add(new AddJsonResponseHeader);
+
+};
