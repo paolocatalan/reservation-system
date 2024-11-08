1e8396d2f8e66af0ecaba5ac1b183bff74ccc007
paolocatalan <paolo_catalan@yahoo.com>
2024-11-08 16:16:34 +0800

    refactor midlleware files

--- public/index.php
+++ public/index.php
@@ -2,11 +2,9 @@
 
 declare(strict_types=1);
 
-use App\Middleware\AddJsonResponseHeader;
 use DI\ContainerBuilder;
 use Dotenv\Dotenv;
 use Slim\Factory\AppFactory;
-use Slim\Handlers\Strategies\RequestResponseArgs;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
@@ -17,17 +15,10 @@ $containerBuilder = new ContainerBuilder();
 $container = $containerBuilder->addDefinitions(dirname(__DIR__) . '/configs/definitions.php')->build();
 
 AppFactory::setContainer($container);
-
 $app = AppFactory::create();
 
-$app->addBodyParsingMiddleware();
-$collector = $app->getRouteCollector();
-$collector->setDefaultInvocationStrategy(new RequestResponseArgs);
-
-$errorMiddlleware = $app->addErrorMiddleware(true, true, true);
-$errorHandler = $errorMiddlleware->getDefaultErrorHandler();
-$errorHandler->forceContentType('application/json');
-$app->add(new AddJsonResponseHeader);
+$middleware = require dirname(__DIR__)  . '/src/Middleware.php';
+$middleware($app, $container);
 
 $router = require dirname(__DIR__) . '/src/Routes.php';
 $router($app);
