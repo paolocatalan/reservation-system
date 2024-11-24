20ca6f971ea5387986c47406ad31119a635df593
paolocatalan <paolo_catalan@yahoo.com>
2024-11-24 12:39:57 +0800

    api endpoint updates

--- src/Routes.php
+++ src/Routes.php
@@ -16,18 +16,18 @@ use Slim\Routing\RouteCollectorProxy;
 
 return function(App $app) {
     $app->group('/api', function (RouteCollectorProxy $group) {
-        $group->get('/orders', [OrderController::class, 'index']);
+        $group->get('/orders', [OrderController::class, 'index'])->add(new AuthMiddleware());
         $group->get('/orders/{orderId:[0-9]+}', [OrderController::class, 'show'])->add(new AuthMiddleware());
 
         $group->post('/orders', [OrderController::class, 'store']);
         $group->post('/orders/dinnings', [RestaurantController::class, 'store']);
 
-        $group->post('/orders/dinnings/', FindTableByNameController::class)->add(new AuthMiddleware());
-        $group->post('/orders/rooms/', FindRoomByNameController::class)->add(new AuthMiddleware());
+        $group->post('/orders/dinnings/names', FindTableByNameController::class)->add(new AuthMiddleware());
+        $group->post('/orders/rooms/names', FindRoomByNameController::class)->add(new AuthMiddleware());
 
-        $group->get('/orders/rooms/', [RoomController::class, 'index'])->add(new AuthMiddleware());
+        $group->get('/orders/rooms', [RoomController::class, 'index'])->add(new AuthMiddleware());
 
-        $group->get('/orders/', GetOrderByDateController::class)->add(new AuthMiddleware());
+        $group->get('/orders/dates', GetOrderByDateController::class)->add(new AuthMiddleware());
 
         $group->post('/register', [AuthController::class, 'store']);
         $group->post('/login', [SessionController::class, 'store']);
